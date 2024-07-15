8bb2e4bfc50bb1d5f3a656ef57f420b3e3952a11
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2024-07-15 17:16:29 +0200

    started project snapgram

---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,65 @@
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { SignUpValidation } from "@/lib/validation";
+
+const SignUpForm = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SignUpValidation>>({
+    resolver: zodResolver(SignUpValidation),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SignUpValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
+        <h2></h2>
+      </div>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignUpForm;
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,17 @@
+import * as z from "zod";
+
+export const SignUpValidation = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Must contain at least 2 characters" })
+    .max(50),
+  username: z
+    .string()
+    .min(2, { message: "Must contain at least 2 characters" })
+    .max(50),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: "Must contain at least 8 characters" })
+    .max(50),
+});
