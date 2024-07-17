e00b034b59c1907cf5a46e57964757d8f5c1b112
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2024-07-17 18:26:16 +0200

    navbar desktop/mobile ok

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import { sidebarLinks } from "@/constants";
+import { InterfaceNavLink } from "@/types";
+
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const BottomBar = () => {
+  const { pathname } = useLocation();
   return (
-    <div>
-      BottomBar
-    </div>
-  )
-}
+    <section className="bottom-bar">
+      {sidebarLinks.map((link: InterfaceNavLink) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={`${isActive && "invert-white"}`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
+};
 
-export default BottomBar
+export default BottomBar;
