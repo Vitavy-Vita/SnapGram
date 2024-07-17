c9bb6141e1f780abd5cd06d17688ef08fb3c200a
Vitavy <127411106+Vitavy-Vita@users.noreply.github.com>
2024-07-17 14:13:04 +0200

    InterfaceContextType

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,49 +1,57 @@
+export type InterfaceContextType = {
+  user: InterfaceUser;
+  isLoading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<InterfaceUser>>;
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+  checkAuthUser: () => Promise<boolean>;
+};
+
 export type InterfaceNavLink = {
-    imgURL: string;
-    route: string;
-    label: string;
-  };
-  
-  export type InterfaceUpdateUser = {
-    userId: string;
-    name: string;
-    bio: string;
-    imageId: string;
-    imageUrl: URL | string;
-    file: File[];
-  };
-  
-  export type InterfaceNewPost = {
-    userId: string;
-    caption: string;
-    file: File[];
-    location?: string;
-    tags?: string;
-  };
-  
-  export type InterfaceUpdatePost = {
-    postId: string;
-    caption: string;
-    imageId: string;
-    imageUrl: URL;
-    file: File[];
-    location?: string;
-    tags?: string;
-  };
-  
-  export type InterfaceUser = {
-    id: string;
-    name: string;
-    username: string;
-    email: string;
-    imageUrl: string;
-    bio: string;
-  };
-  
-  export type InterfaceNewUser = {
-    name: string;
-    email: string;
-    username: string;
-    password: string;
-  };
-  
+  imgURL: string;
+  route: string;
+  label: string;
+};
+
+export type InterfaceUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
+export type InterfaceNewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type InterfaceUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type InterfaceUser = {
+  id: string;
+  name: string;
+  username: string;
+  email: string;
+  imageUrl: string;
+  bio: string;
+};
+
+export type InterfaceNewUser = {
+  name: string;
+  email: string;
+  username: string;
+  password: string;
+};
